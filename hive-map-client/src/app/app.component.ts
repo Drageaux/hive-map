114774e8cf8f617ecfc802ceccabd37ecc13bf2f
David Thong Nguyen <dev@davidtn.com>
2020-10-01 00:45:18 -0400

    Fabric setup

--- hive-map-client/src/app/app.component.ts
+++ hive-map-client/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'hive-map-client';
+export class AppComponent implements OnInit {
+  canvas: any;
+
+  ngOnInit() {
+    this.canvas = new fabric.Canvas('canvas');
+    this.canvas.add(new fabric.IText('Hello World !'));
+  }
 }
