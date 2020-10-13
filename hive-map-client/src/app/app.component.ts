4bfcb77028dfd3bbe36d44032c11b2181d57fd72
David Thong Nguyen <dev@davidtn.com>
2020-10-13 00:05:57 -0400

    Translating code slowly

--- hive-map-client/src/app/app.component.ts
+++ hive-map-client/src/app/app.component.ts
@@ -13,6 +13,16 @@ export class AppComponent implements OnInit {
   currMessage = 'test';
   data: Message = exampleData;
 
+  // d3 set up
+  tree = d3.tree().size([1000, 1000]);
+  diagonal = d3
+    .linkHorizontal()
+    .x((d) => d[0])
+    .y((d) => d[1]); // node paths
+  root: d3.HierarchyNode<Message> = exampleData;
+  // svg-related objects
+  svg;
+
   // calculate total nodes, max label length
   totalNodes = 0;
   maxLabelLength = 0;
@@ -22,20 +32,20 @@ export class AppComponent implements OnInit {
   // panning variables
   panSpeed = 200;
   panBoundary = 20; // Within 20px from edges will pan when dragging.
-  // Misc. variables
+  // misc. variables
   i = 0;
   duration = 750;
-  root;
 
   ngOnInit() {
-    console.log(this.data);
-    let tree = d3.tree().size([1000, 1000]);
-
-    // node paths
-    let diagonal = d3
-      .linkHorizontal()
-      .x((d) => d[0])
-      .y((d) => d[1]);
+    // size of the diagram
+    var viewerWidth = window.innerWidth;
+    var viewerHeight = window.innerHeight;
+    this.svg = d3
+      .select('#hive-map')
+      .attr('width', viewerWidth)
+      .attr('height', viewerHeight)
+      .attr('class', 'overlay');
+    // .call(zoomListener);
 
     let recurVisit = (parentMessage, visitFn, childrenFn) => {
       if (!parentMessage) {
@@ -65,6 +75,8 @@ export class AppComponent implements OnInit {
 
     // use the above functions to visit and establish maxLabelLength
     recurVisit(this.data, visit, getNextChildren);
+
+    this.update(this.root);
   }
 
   /*************************************************************************/
@@ -79,5 +91,191 @@ export class AppComponent implements OnInit {
     //   'transform',
     //   'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')'
     // );
+  }
+
+  /*************************************************************************/
+  /****************************** DATA UPDATE ******************************/
+  /*************************************************************************/
+  update() {
+    // Compute the new height, function counts total children of root node and sets tree height accordingly.
+    // This prevents the layout looking squashed when new nodes are made visible or looking sparse when nodes are removed
+    // This makes the layout more consistent.
+    let levelWidth = [1];
+    let childCount = function (level, n) {
+      if (n.children && n.children.length > 0) {
+        if (levelWidth.length <= level + 1) levelWidth.push(0);
+
+        levelWidth[level + 1] += n.children.length;
+        n.children.forEach(function (d) {
+          childCount(level + 1, d);
+        });
+      }
+    };
+    childCount(0, this.currMessage);
+    var newHeight = d3.max(levelWidth) * 25; // 25 pixels per line
+    this.tree = this.tree.size([newHeight, window.innerWidth]);
+
+    // Compute the new tree layout.
+    const treeRoot = d3.hierarchy(this.root);
+    const nodes = treeRoot.descendants();
+    const links = treeRoot.links();
+    console.log(treeRoot);
+    console.log(nodes);
+    console.log(links);
+
+    // Set widths between levels based on maxLabelLength.
+    // nodes.forEach((d) => {
+    //   d.y = d.depth * (this.maxLabelLength * 10); //maxLabelLength * 10px
+    //   // alternatively to keep a fixed scale one can set a fixed depth per level
+    //   // Normalize for fixed-depth by commenting out below line
+    //   // d.y = (d.depth * 500); //500px per level.
+    // });
+
+    // Update the nodes…
+    // node = svgGroup.selectAll('g.node').data(nodes, function (d) {
+    //   return d.id || (d.id = ++i);
+    // });
+
+    // Enter any new nodes at the parent's previous position.
+    // var nodeEnter = node
+    //   .enter()
+    //   .append('g')
+    //   .call(dragListener)
+    //   .attr('class', 'node')
+    //   .attr('transform', function (d) {
+    //     return 'translate(' + source.y0 + ',' + source.x0 + ')';
+    //   })
+    //   .on('click', click);
+
+    // nodeEnter
+    //   .append('circle')
+    //   .attr('class', 'nodeCircle')
+    //   .attr('r', 0)
+    //   .style('fill', function (d) {
+    //     return d._children ? 'lightsteelblue' : '#fff';
+    //   });
+
+    // nodeEnter
+    //   .append('text')
+    //   .attr('x', function (d) {
+    //     return d.children || d._children ? -10 : 10;
+    //   })
+    //   .attr('dy', '.35em')
+    //   .attr('class', 'nodeText')
+    //   .attr('text-anchor', function (d) {
+    //     return d.children || d._children ? 'end' : 'start';
+    //   })
+    //   .text(function (d) {
+    //     return d.name;
+    //   })
+    //   .style('fill-opacity', 0);
+
+    // phantom node to give us mouseover in a radius around it
+    // nodeEnter
+    //   .append('circle')
+    //   .attr('class', 'ghostCircle')
+    //   .attr('r', 30)
+    //   .attr('opacity', 0.2) // change this to zero to hide the target area
+    //   .style('fill', 'red')
+    //   .attr('pointer-events', 'mouseover')
+    //   .on('mouseover', function (node) {
+    //     overCircle(node);
+    //   })
+    //   .on('mouseout', function (node) {
+    //     outCircle(node);
+    //   });
+
+    // Update the text to reflect whether node has children or not.
+    // node
+    //   .select('text')
+    //   .attr('x', function (d) {
+    //     return d.children || d._children ? -10 : 10;
+    //   })
+    //   .attr('text-anchor', function (d) {
+    //     return d.children || d._children ? 'end' : 'start';
+    //   })
+    //   .text(function (d) {
+    //     return d.name;
+    //   });
+
+    // Change the circle fill depending on whether it has children and is collapsed
+    // node
+    //   .select('circle.nodeCircle')
+    //   .attr('r', 4.5)
+    //   .style('fill', function (d) {
+    //     return d._children ? 'lightsteelblue' : '#fff';
+    //   });
+
+    // Transition nodes to their new position.
+    // var nodeUpdate = node
+    //   .transition()
+    //   .duration(duration)
+    //   .attr('transform', function (d) {
+    //     return 'translate(' + d.y + ',' + d.x + ')';
+    //   });
+
+    // Fade the text in
+    // nodeUpdate.select('text').style('fill-opacity', 1);
+
+    // Transition exiting nodes to the parent's new position.
+    // var nodeExit = node
+    //   .exit()
+    //   .transition()
+    //   .duration(duration)
+    //   .attr('transform', function (d) {
+    //     return 'translate(' + source.y + ',' + source.x + ')';
+    //   })
+    //   .remove();
+
+    // nodeExit.select('circle').attr('r', 0);
+
+    // nodeExit.select('text').style('fill-opacity', 0);
+
+    // Update the links…
+    // var link = svgGroup.selectAll('path.link').data(links, function (d) {
+    //   return d.target.id;
+    // });
+
+    // Enter any new links at the parent's previous position.
+    // link
+    //   .enter()
+    //   .insert('path', 'g')
+    //   .attr('class', 'link')
+    //   .attr('d', function (d) {
+    //     var o = {
+    //       x: source.x0,
+    //       y: source.y0,
+    //     };
+    //     return diagonal({
+    //       source: o,
+    //       target: o,
+    //     });
+    //   });
+
+    // Transition links to their new position.
+    // link.transition().duration(duration).attr('d', diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    // link
+    //   .exit()
+    //   .transition()
+    //   .duration(duration)
+    //   .attr('d', function (d) {
+    //     var o = {
+    //       x: source.x,
+    //       y: source.y,
+    //     };
+    //     return diagonal({
+    //       source: o,
+    //       target: o,
+    //     });
+    //   })
+    //   .remove();
+
+    // // Stash the old positions for transition.
+    // nodes.forEach(function (d) {
+    //   d.x0 = d.x;
+    //   d.y0 = d.y;
+    // });
   }
 }
